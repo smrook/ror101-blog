c06c1751c12fb24ee5670e93b36f894ecbb8a58c
mrook <maven.lu@gmail.com>
2014-04-05 01:52:11 +0000

    recommit with new gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Blog::Application.routes.draw do
-  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,9 +12,7 @@ Blog::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :posts do
-    resources :comments
-  end
+  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
